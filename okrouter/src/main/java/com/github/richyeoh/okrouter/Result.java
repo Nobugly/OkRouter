7c46184cf90788e3ec0cc3e4e26eee483592d640
Richyeoh <richeyoh@foxmail.com>
2018-09-07 02:27:01 +0800

    Implements Result class

--- okrouter/src/main/java/com/github/richyeoh/okrouter/Result.java
+++ okrouter/src/main/java/com/github/richyeoh/okrouter/Result.java
@@ -1,4 +1,29 @@
 package com.github.richyeoh.okrouter;
 
+import android.content.Intent;
+
 public final class Result {
+    private int mResultCode;
+    private Intent mData;
+
+    public Result(int resultCode, Intent data) {
+        mResultCode = resultCode;
+        mData = data;
+    }
+
+    public int getResultCode() {
+        return mResultCode;
+    }
+
+    public void setResultCode(int resultCode) {
+        mResultCode = resultCode;
+    }
+
+    public Intent getData() {
+        return mData;
+    }
+
+    public void setData(Intent data) {
+        mData = data;
+    }
 }
